b1abe872557d344980f0487e7430559623889021
Hansatang <portokarlos@gmail.com>
2021-04-22 12:52:07 +0200

    Init

--- src/client/view/frontPage/FrontPageController.java
+++ src/client/view/frontPage/FrontPageController.java
@@ -2,48 +2,53 @@ package client.view.frontPage;
 
 import client.view.ViewHandler;
 import client.viewmodel.frontPage.UserFrontPageViewModel;
-import client.viewmodel.login.LoginViewModel;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
-import javafx.scene.image.Image;
-
-import java.io.File;
-
-public class FrontPageController {
-
-    private UserFrontPageViewModel userFrontPageViewModel;
-    private ViewHandler viewHandler;
-
-    @FXML private Label usernameLabel;
-    @FXML private Button loginButton;
-    @FXML private TableView movieTableView;
-    @FXML private TableColumn movieTitleCol;
-    @FXML private TableColumn dateOfReleaseCol;
+import javafx.scene.control.cell.PropertyValueFactory;
+import shared.Movie;
 
 
+public class FrontPageController
+{
 
-  public void init(UserFrontPageViewModel frontPage,ViewHandler viewHandler)
+  private UserFrontPageViewModel userFrontPageViewModel;
+  private ViewHandler viewHandler;
+
+  @FXML private Label usernameLabel;
+  @FXML private Button loginButton;
+  @FXML private TableView<Movie> movieTableView;
+  @FXML private TableColumn<String, Movie> movieTitleCol;
+  @FXML private TableColumn<String, Movie> dateOfReleaseCol;
+
+  public void init(UserFrontPageViewModel frontPage, ViewHandler viewHandler)
   {
-
     this.userFrontPageViewModel = frontPage;
     this.viewHandler = viewHandler;
 
-    usernameLabel.textProperty().bindBidirectional(userFrontPageViewModel.usernameProperty());
+    usernameLabel.textProperty()
+        .bindBidirectional(userFrontPageViewModel.usernameProperty());
+    movieTableView.itemsProperty().bindBidirectional(userFrontPageViewModel.getNewItem);
+    movieTitleCol.setCellValueFactory(new PropertyValueFactory<>("name"));
+    dateOfReleaseCol
+        .setCellValueFactory(new PropertyValueFactory<>("dateOfRelease"));
 
-      /*userFrontPageViewModel.selectedItemProperty().bind(
-              table.getSelectionModel().selectedItemProperty());
-*/
-
+    movieTableView.setItems(userFrontPageViewModel.getItems());
+    System.out.println("369" + movieTableView.getItems().toString());
   }
 
+  public void onLoginButton(ActionEvent event)
+  {
+    viewHandler.openLoginView();
+  }
 
+  public void onBookMovieButton()
+  {
+    movieTableView.setItems(userFrontPageViewModel.getItems());
+  }
 
-
-    public void onLoginButton(ActionEvent event) {
-      viewHandler.openLoginView();
-    }
 }
--- src/client/viewmodel/frontPage/UserFrontPageViewModel.java
+++ src/client/viewmodel/frontPage/UserFrontPageViewModel.java
@@ -1,44 +1,74 @@
 package client.viewmodel.frontPage;
 
 import client.model.UserModel;
-import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.property.StringProperty;
-import shared.User;
+import javafx.beans.property.*;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import shared.Movie;
+import shared.util.EventType;
 
-public class UserFrontPageViewModel {
+import java.beans.PropertyChangeEvent;
+import java.util.ArrayList;
+import java.util.List;
 
+public class UserFrontPageViewModel
+{
 
-    private UserModel userModel;
+  public Property<ObservableList<Movie>> getNewItem;
+  private UserModel model;
 
-    private StringProperty username,button;
-  /*  private ObservableList<ContactTableRow> items = ...
-    private ObjectProperty<ContactTableRow> selectedItem
+  private StringProperty username, button;
+  private ObservableList<Movie> items;
 
-*/
-    public UserFrontPageViewModel(UserModel userModel){
-        this.userModel = userModel;
-      username = new SimpleStringProperty();
-      button=new SimpleStringProperty();
+  public UserFrontPageViewModel(UserModel userModel)
+  {
+    this.model = userModel;
+    username = new SimpleStringProperty();
+    button = new SimpleStringProperty();
+    items = new SimpleListProperty<>();
+    model.addPropertyChangeListener(EventType.GETMOVIES_RESULT.toString(),
+        this::onGetMovies);
 
-    }
+    System.out.println(5);
+  }
 
+  private void onGetMovies(PropertyChangeEvent event)
+  {
+    System.out.println(6);
+    List<Movie> list = (ArrayList<Movie>) event.getNewValue();
 
+    items = FXCollections.observableArrayList(list);
 
+    System.out.println("HAHA" + items.toString());
+  }
 
+  public StringProperty usernameProperty()
+  {
+    return username;
+  }
 
+  public StringProperty buttonProperty()
+  {
+    return button;
+  }
 
-    public StringProperty usernameProperty() {
-        return username;
-    }
+  public void getMovies()
+  {
 
+  }
 
-    public StringProperty buttonProperty() {
-        return button;
-    }
+  public ObservableList<Movie> getItems()
+  {
+    return items;
+  }
 
-    public void getMovies() {
+  public ObservableList<Movie> getGetNewItem()
+  {
+    return getNewItem.get();
+  }
 
-
-
-    }
+  public Property<ObservableList<Movie>> getNewItemProperty()
+  {
+    return getNewItem;
+  }
 }
