8457777990def0d0bce447436e29efc01d0d8af4
Hansatang <portokarlos@gmail.com>
2021-05-27 11:47:58 +0200

    Pacierz

--- src/client/core/ClientFactory.java
+++ src/client/core/ClientFactory.java
@@ -3,16 +3,13 @@ package client.core;
 import client.networking.ClientImpl;
 import client.networking.Client;
 
-public class ClientFactory
-{
-  private ClientImpl client;
+public class ClientFactory {
+    private ClientImpl client;
 
-  public ClientImpl getClient()
-  {
-    if (client == null)
-    {
-      client = new Client();
+    public ClientImpl getClient() {
+        if (client == null) {
+            client = new Client();
+        }
+        return client;
     }
-    return client;
-  }
 }
--- src/client/core/ModelFactory.java
+++ src/client/core/ModelFactory.java
@@ -3,24 +3,18 @@ package client.core;
 import client.model.Model;
 import client.model.UserModel;
 
+public class ModelFactory {
+    private UserModel userModel;
+    private ClientFactory clientFactory;
 
-
-public class ModelFactory
-{
-  private UserModel userModel;
-  private ClientFactory clientFactory;
-
-  public ModelFactory(ClientFactory clientFactory)
-  {
-    this.clientFactory = clientFactory;
-  }
-
-  public UserModel getUserModel()
-  {
-    if (userModel == null)
-    {
-      userModel = new Model(clientFactory.getClient());
+    public ModelFactory(ClientFactory clientFactory) {
+        this.clientFactory = clientFactory;
     }
-    return userModel;
-  }
+
+    public UserModel getUserModel() {
+        if (userModel == null) {
+            userModel = new Model(clientFactory.getClient());
+        }
+        return userModel;
+    }
 }
